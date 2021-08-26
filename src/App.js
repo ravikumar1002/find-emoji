f952c24eb4de03fedc052e4f1af282eb69e80c97
Ravi kumar <86847625+reactive-alchemy@users.noreply.github.com>
2021-08-26 23:33:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😃": "Smileys & People",
+  "🐻": "Animals & Nature",
+  "🍔": "Food & Drink",
+  "⚽": "Activity",
+  "🌇": "Travel & Places",
+  "💡": "Objects",
+  "🔣": "Symbols",
+  "🎌": "Flags"
+};
+
+var emojiKey = Object.keys(emojiDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+
+  function emojisearchHandler(event) {
+    var inputEmoji = event.target.value;
+    var searchEmoji = emojiDictionary[inputEmoji];
+    setMeaning(searchEmoji);
+  }
+
+  function emojiClickHandler(emoji) {
+    var searchEmoji = emojiDictionary[emoji];
+    setMeaning(searchEmoji);
+  }
+
+  return (
+    <div className="container-body">
+      <div className="App">
+        <h1>Find Your Emoji </h1>
+        <input onChange={emojisearchHandler} />
+        <h2>{meaning}</h2>
+
+        <h3>Types of emoji</h3>
+        {emojiKey.map(function (emoji) {
+          return (
+            <span
+              onClick={() => emojiClickHandler(emoji)}
+              style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+              key={emoji}
+            >
+              {" "}
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
